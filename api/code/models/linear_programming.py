412be8db9581c71adac56ba77a96f3cd51fb2cd4
Alberto Morales <mail@albertomorales.eu>
2024-10-29 08:14:32 +0100

    solver finished

---
+++ api/code/models/linear_programming.py
@@ -0,0 +1,59 @@
+"""
+    This class provide the abstraction of the LinearProgrammingProblem and ProblemResult data structures.
+
+
+
+"""
+
+
+import logging
+from abc import ABC, abstractmethod
+
+from config.constants import BS_LOGGER
+
+class LinearProgrammingProblem():
+    def __init__(self, objective_function=None, contraint_matrix=None, constraint_vector=None, bounds=None):
+        """Linear programming problem (simplyfied) data sructure.
+
+        Parameters
+            ----------
+            objective_function : 1-D array
+                The coefficients of the linear objective function to be minimized.
+            contraint_matrix : 2-D array, optional
+                The equality constraint matrix. Each row of ``contraint_matrix`` specifies the
+                coefficients of a linear equality constraint on ``x``.
+            constraint_vector : 1-D array, optional
+                The equality constraint vector. Each element of ``contraint_matrix @ x`` must equal
+                the corresponding element of ``constraint_vector``.
+            bounds : sequence, optional
+                A sequence of ``(min, max)`` pairs for each element in ``x``, defining
+                the minimum and maximum values of that decision variable. Use ``None``
+                to indicate that there is no bound. By default, bounds are
+                ``(0, None)`` (all decision variables are non-negative).
+        """    
+        self._logger = logging.getLogger(BS_LOGGER)
+        self.objective_function = objective_function
+        self.contraint_matrix = contraint_matrix
+        self.constraint_vector = constraint_vector
+        self.bounds = bounds
+
+class ProblemResult():
+    def __init__(self, solution, opt, status):          
+        """Linear programming problem result (simplyfied) data sructure.
+
+        Parameters
+        ----------
+        solution : 1-D array
+            The values of the decision variables that minimizes the
+            objective function while satisfying the constraints.
+        opt : float
+            The optimal value of the objective function ``c @ x``.
+        status : int
+            An integer representing the exit status of the solving operation.
+            ``0`` : Terminated successfully.
+            ``-1`` : Terminated unsuccessfully.            
+        """          
+        self._logger = logging.getLogger(BS_LOGGER)
+        self.solution = solution
+        self.opt = opt
+        self.status = status
--- api/code/solver/solver_impl.py
+++ api/code/solver/solver_impl.py
@@ -4,6 +4,7 @@ from config.constants import BS_LOGGER
 from solver.solver import Solver
 from models.linear_programming import LinearProgrammingProblem, ProblemResult
 
+from scipy.optimize import linprog 
 
 class SolverImpl(Solver):
     def __init__(self):
@@ -11,5 +12,15 @@ class SolverImpl(Solver):
         self._logger = logging.getLogger(BS_LOGGER)
 
     def solve(self, problem: LinearProgrammingProblem) -> ProblemResult :
-        result = ProblemResult()
+        self._logger.debug(f"Objective function coeficients: {problem.objective_function}")
+        self._logger.debug(f"Constraint matrix: {problem.contraint_matrix}")
+        self._logger.debug(f"Constraint vector: {problem.constraint_vector}")
+        self._logger.debug(f"Bounds: {problem.bounds}")
+        solution = linprog(c=problem.objective_function, 
+                          A_eq=problem.contraint_matrix, 
+                          b_eq=problem.constraint_vector, 
+                          bounds=problem.bounds, 
+                          method='highs-ds')
+        self._logger.debug(solution.message)
+        result = ProblemResult(solution=solution.x, opt=solution.fun, status=solution.status)
         return result
